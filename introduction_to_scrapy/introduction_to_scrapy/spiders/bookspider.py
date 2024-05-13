4ae43c2d8557d70a8fd9e88dc92d4a21405f30c4
soszaboss <moustoifa10@outlook.fr>
2024-05-13 17:54:05 +0000

    fixing proxy

--- introduction_to_scrapy/introduction_to_scrapy/spiders/bookspider.py
+++ introduction_to_scrapy/introduction_to_scrapy/spiders/bookspider.py
@@ -1,16 +1,45 @@
 import scrapy
 from introduction_to_scrapy.items import BookItems
+from urllib.parse import urlencode
+import introduction_to_scrapy.settings as settings
+
+def get_proxy_url(url: str):
+    return f'https://proxy.scrapeops.io/v1/?api_key={settings.SCRAPEOPS_API_KEY}&url={url}'
+
 
 class BookspiderSpider(scrapy.Spider):
     name = "bookspider"
-    allowed_domains = ["books.toscrape.com"]
+    allowed_domains = ["books.toscrape.com", "proxy.scrapeops.io"]
     start_urls = ["https://books.toscrape.com/"]
+    sops_job_name = "scrape books to scrape"
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url = get_proxy_url(self.start_urls[0]),
+            callback = self.parse
+        )
 
     def parse(self, response):
         links =  response.css('article h3 a')
-        yield from response.follow_all(links, callback=self.parse_book_page)
-        pagination_links = response.css("li.next a")
-        yield from response.follow_all(pagination_links, self.parse)
+        links = [link.attrib['href'] for link in links]
+        for i in range(len(links)):
+            if not 'catalogue' in links[i]:
+                links[i] = f'catalogue/{links[i]}'
+        links = [f'https://books.toscrape.com/{link}' for link in links]
+        requests = [scrapy.Request(url=get_proxy_url(link), callback=self.parse_book_page) for link in links]
+
+        # Itérez sur les objets Request
+        for request in requests:
+            yield request
+
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            if 'catalogue/' in next_page:
+                next_page = f'https://books.toscrape.com/{next_page}'
+            else:
+                next_page = f'https://books.toscrape.com/catalogue/{next_page}'
+            yield scrapy.Request(url = get_proxy_url(next_page), callback=self.parse)
 
     def parse_book_page(self, response):
         book_items = BookItems()
